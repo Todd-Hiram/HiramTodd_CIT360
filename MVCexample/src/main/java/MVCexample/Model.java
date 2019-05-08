15df3b1ba27d4ae849a6c9872f5abb1a33cb9fdd
Hiram Todd <htillustrator@gmail.com>
2019-05-07 19:22:18 -0700

    Small revisions.

--- MVCexample/src/main/java/MVCexample/Model.java
+++ MVCexample/src/main/java/MVCexample/Model.java
@@ -13,9 +13,17 @@ package MVCexample;
 public class Model {
     private int calValue;
     
-    public void addTwoNum(int firstNum, int secondNum) {
-        calValue = firstNum + secondNum;
+    public void multiplyTwoNum(int firstNum, int secondNum) {
+        calValue = firstNum * secondNum;
     }
+    
+    /****************************************************************************
+    * The Model of the MVC represents an object that contains data for an 
+    * application. This part of the architecture handles the data and the business
+    * logic for the program. When a user submits a question, the Contoller 
+    * contacts the Model with the appropriate data and brings the data to the 
+    * view of the user.
+    *****************************************************************************/
     
     public int getCalcValue() {
         return calValue;
