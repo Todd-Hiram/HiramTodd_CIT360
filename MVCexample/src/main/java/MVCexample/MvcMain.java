96221666ac764a3d8ffb68d79ea6f612a77c3e30
Hiram Todd <htillustrator@gmail.com>
2019-05-07 18:38:03 -0700

    Small revisions.

---
+++ MVCexample/src/main/java/MVCexample/MvcMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVCexample;
+
+//import javax.swing.text.View;
+
+/**
+ *
+ * @author hiramtodd
+ */
+public class MvcMain {
+    public static void main(String[] args) {
+        View theView = new View();
+        Model theModel = new Model();
+        Controller theController = new Controller(theView, theModel);
+        theView.setVisible(true);
+    }
+}
