bb0a4f769bef4ce4ff12d4bd700d444067cde20d
Hiram Todd <htillustrator@gmail.com>
2019-06-14 23:46:43 -0700

    Commit Application Controller Pattern 06/14/2019

---
+++ ApplicationControllerPattern3/src/java/applicationcontrollerpattern3/Command.java
@@ -0,0 +1,20 @@
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applicationcontrollerpattern3;
+
+import org.springframework.web.servlet.support.RequestContext;
+
+/**
+ *
+ * @author hiramtodd
+ */
+public interface Command {
+    
+    String execute(RequestContext requestContext);
+
+    public String execute(applicationcontrollerpattern3.RequestContext requestContext);
+    
+}
