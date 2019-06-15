96537587e7c933f557bc67aa9fd73da62c917a1e
Hiram Todd <htillustrator@gmail.com>
2019-06-14 23:48:06 -0700

    Commit Application Controller Pattern 06/14/2019

---
+++ ApplicationControllerPattern3/src/java/applicationcontrollerpattern3/Dispatcher.java
@@ -0,0 +1,37 @@
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applicationcontrollerpattern3;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author hiramtodd
+ */
+public class Dispatcher {
+    public void dispatch(HttpServletRequest request, HttpServletResponse response, String page) throws IOException {
+        RequestDispatcher rd = null;
+        
+        rd = request.getRequestDispatcher(page);
+        try {
+            
+            rd.forward(request, response);
+            
+        } catch(ServletException e) {
+         
+            e.printStackTrace();
+            
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+            
+        }
+    }
+}
